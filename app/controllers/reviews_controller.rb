e79fcdeb2124466ec3d3681ea34a46d1644f3ab7
Sean Donnelly <sean_donnelly@hotmail.co.uk>
2024-02-21 16:44:45 +0000

    Linked up pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,22 @@
 class ReviewsController < ApplicationController
+
   def new
     # We need @restaurant in our `simple_form_for`
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant_id = @restaurant.id
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
